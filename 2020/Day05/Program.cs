749f24db52be4e488886418b7296761402c78954
jon.softeland <jon.softeland@gmail.com>
2020-12-06 11:26:32 +0100

    Day 05 - fix

--- 2020/Day05/Program.cs
+++ 2020/Day05/Program.cs
@@ -2,13 +2,9 @@
 using System.IO;
 using System.Linq;
 
-var tickets = File.ReadAllLines("input.txt");
-Console.WriteLine(tickets.Select(t => SeatValue(t)).Max());
+Console.WriteLine(File.ReadAllLines("input.txt").Select(t => SeatValue(t)).Max());
+var seats = File.ReadAllLines("input.txt").Select(t => SeatValue(t)).OrderBy(v => v).ToList();
+Console.WriteLine(seats.Skip(1).Where((s, i) => s > seats[i]+1).First()-1);
 
-var seats = tickets.Select(t => SeatValue(t)).OrderBy(v => v).ToList();
-Console.WriteLine(seats.Take(seats.Count() - 1).Where((s, i) => seats[i + 1] > s + 1).First()+1);
-
-int SeatValue(string input) => Column(input) * 8 + Row(input);
-int Column(string input) => ToInt(input[..7], 'B');
-int Row(string input) => ToInt(input[^3..], 'R');
-int ToInt(string input, char c) => input.Aggregate(0, (t, n) => n == c ? ((t << 1) + 1) : t << 1);
+int SeatValue(string input) => ToInt(input[..7], 'B') * 8 + ToInt(input[^3..], 'R');
+int ToInt(string input, char c) => input.Aggregate(0, (t, n) => n == c  ? ((t << 1) + 1) : t << 1);
