cba0dc06fc69d05c7c2abda1ac2be9579144bdba
jon.softeland <jon.softeland@gmail.com>
2020-12-06 13:33:06 +0100

    Day 06 - fix

--- 2020/Day06/Program.cs
+++ 2020/Day06/Program.cs
@@ -7,5 +7,5 @@ var questions = File.ReadAllText("input.txt").Split("\r\n\r\n").Select(l => l.Sp
 IEnumerable<char> AllAnswers(string[] group) => group.SelectMany(y => y).Distinct();
 IEnumerable<char> EveryoneAnswers(string[] group) => AllAnswers(group).Where(y => group.All(c => c.Contains(y)));
 
-Console.WriteLine(questions.Select(x => AllAnswers(x)).Select(x => x.Count()).Sum());
-Console.WriteLine(questions.Select(x => EveryoneAnswers(x)).Select(x => x.Count()).Sum());
+Console.WriteLine(questions.Select(x => AllAnswers(x).Count()).Sum());
+Console.WriteLine(questions.Select(x => EveryoneAnswers(x).Count()).Sum());
