5e4e4fdfc24235edf58b7fafd586bcb63f0aec19
jon.softeland <jon.softeland@gmail.com>
2020-12-07 18:23:53 +0100

    Day 07

---
+++ 2020/Day07/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var values = File.ReadAllLines("input.txt").Select(t => t
+   .Split(new string[] { "contain", ",", "." }, StringSplitOptions.RemoveEmptyEntries)
+   .Select(x => x.Trim().Replace("bags", "bag")).ToList())
+      .ToDictionary(
+         k => k[0],
+         v => v[1].Trim() != "no other bag" ? 
+            v.Skip(1).ToDictionary(v => v.Substring(2), v => Int32.Parse(v.Substring(0, 1))) : 
+            new Dictionary<string, int>());
+
+bool Contains(string bag) => values[bag].ContainsKey("shiny gold bag") || values[bag].Keys.Any(Contains);
+int BagsContaining(string bag) => values[bag].Sum(kvp => kvp.Value * (BagsContaining(kvp.Key) + 1));
+
+Console.WriteLine(values.Keys.Count(x => Contains(x)));
+Console.WriteLine(BagsContaining("shiny gold bag"));
