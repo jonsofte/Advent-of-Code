02d0daf80f5df89dfe0627ae27bafab60641cbed
jon.softeland <jon.softeland@gmail.com>
2020-12-09 19:43:16 +0100

    Day04 - fix

--- 2020/Day04/Program.cs
+++ 2020/Day04/Program.cs
@@ -9,18 +9,18 @@ var passports = File.ReadAllText("input.txt").Split("\r\n\r\n")
          .Select(s => s.Split(':')).Select(x => (key: x[0], value: x[1]))).ToList();
 
 var ValidPassports = passports.Where(x => x.Count(y => y.key != "cid") == 7);
-
 Console.WriteLine(ValidPassports.Count());
 Console.WriteLine(ValidPassports.Where(x => x.All(y => isValidValue(y.key, y.value))).Count());
 
 bool isValidValue(string key, string value) => key switch
-   {
+{
    "byr" => Int32.Parse(value) is >= 1920 and <= 2002,
    "iyr" => Int32.Parse(value) is >= 2010 and <= 2020,
    "eyr" => Int32.Parse(value) is >= 2020 and <= 2030,
-      "hgt" => Regex.IsMatch(value, "(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)$"),
-      "hcl" => Regex.IsMatch(value, "#([0-9]|[a-f]){6}"),
-      "ecl" => new List<string> { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" }.Contains(value),
-      "pid" => value.Length == 9,
-      "cid" => true
-   };
+   "hgt" => Regex.IsMatch(value, "(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)$"),
+   "hcl" => Regex.IsMatch(value, "#([0-9]|[a-f]){6}"),
+   "ecl" => new List<string> { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" }.Contains(value),
+   "pid" => value.Length == 9,
+   "cid" => true,
+   _ => false
+};
