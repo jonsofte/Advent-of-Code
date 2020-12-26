fabbcb5feb7d67bac5fd773025a4da68887c5544
jon.softeland <jon.softeland@gmail.com>
2020-12-26 15:47:57 +0100

    Day09 - fix

--- 2020/Day09/Program.cs
+++ 2020/Day09/Program.cs
@@ -1,27 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 var values = File.ReadAllLines("input.txt").Select(t => long.Parse(t)).ToArray();
-
 long result = Part1();
 Console.WriteLine(result);
-
-long part2 = Part2(result);
-Console.WriteLine(part2);
-
-long Part2(long result)
-{
-   for (int x = 0; x < values.Length+1; x++)
-   {
-      for (int y = x+1; y < values.Length; y++)
-      {
-         if (values[(x)..(y)].Sum()== result) return (values[(x)..(y)].Min() + values[(x)..(y)].Max());
-      }
-   }
-   return 0;
-}
+Console.WriteLine(Part2(result));
 
 long Part1() 
 { 
@@ -42,4 +26,16 @@ bool isSum(long check, long[] preamble)
       }
    }
    return false;
+}
+
+long Part2(long result)
+{
+   for (int x = 0; x < values.Length + 1; x++)
+   {
+      for (int y = x + 1; y < values.Length; y++)
+      {
+         if (values[(x)..(y)].Sum() == result) return (values[(x)..(y)].Min() + values[(x)..(y)].Max());
+      }
+   }
+   return 0;
 }
