703551853cb5e02ec9d96eead98b1bf5425389e5
jon.softeland <jon.softeland@gmail.com>
2020-12-26 15:47:45 +0100

    Day07 - fix

--- 2020/Day07/Program.cs
+++ 2020/Day07/Program.cs
@@ -6,14 +6,15 @@ using System.Linq;
 var values = File.ReadAllLines("input.txt").Select(t => t
    .Split(new string[] { "contain", ",", "." }, StringSplitOptions.RemoveEmptyEntries)
    .Select(x => x.Trim().Replace("bags", "bag")).ToList())
-      .ToDictionary(
-         k => k[0],
-         v => v[1].Trim() != "no other bag" ?
-            v.Skip(1).ToDictionary(v => v[2..], v => int.Parse(v.Substring(0, 1))) :
-            new Dictionary<string, int>());
-
-bool Contains(string bag) => values[bag].ContainsKey("shiny gold bag") || values[bag].Keys.Any(Contains);
-int BagsContaining(string bag) => values[bag].Sum(kvp => kvp.Value * (BagsContaining(kvp.Key) + 1));
+   .ToDictionary( 
+      k => k[0], 
+      v => v[1].Trim() != "no other bag" ? v.Skip(1).ToDictionary(
+         v => v[2..], 
+         v => int.Parse(v.Substring(0, 1))) : new Dictionary<string, int>()
+         );
 
 Console.WriteLine(values.Keys.Count(x => Contains(x)));
 Console.WriteLine(BagsContaining("shiny gold bag"));
+
+bool Contains(string bag) => values[bag].ContainsKey("shiny gold bag") || values[bag].Keys.Any(Contains);
+int BagsContaining(string bag) => values[bag].Sum(kvp => kvp.Value * (BagsContaining(kvp.Key) + 1));
