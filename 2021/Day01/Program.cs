08e96bd7a785323b071c49c735ef2b45b0bd80d6
jon.softeland <jon.softeland@gmail.com>
2021-12-02 22:20:27 +0100

    Day01 - clean up

--- 2021/Day01/Program.cs
+++ 2021/Day01/Program.cs
@@ -1,11 +1,13 @@
 ﻿// https://adventofcode.com/2021/day/1
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 var input = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToList();
+
+Console.WriteLine(NumberOfIncreasingDepths(input));
+Console.WriteLine(NumberOfIncreasingDepths(SlidingWindow(input)));
 
 static int NumberOfIncreasingDepths(List<int> depths) => depths.Skip(1)
     .Zip(depths, (x, y) => x - y)
@@ -14,6 +16,3 @@ static int NumberOfIncreasingDepths(List<int> depths) => depths.Skip(1)
 static List<int> SlidingWindow(List<int> readings) => Enumerable.Range(2, readings.Count - 2)
     .Select(x => readings[x - 2] + readings[x - 1] + readings[x])
     .ToList();
-
-Console.WriteLine(NumberOfIncreasingDepths(input));
-Console.WriteLine(NumberOfIncreasingDepths(SlidingWindow(input)));
