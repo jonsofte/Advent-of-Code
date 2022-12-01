cbb633cdf769912f6430302c96e3538fa0c0d3c4
Jon Øystein <jon.softeland@gmail.com>
2022-12-01 21:27:53 +0100

    2022 Day01

---
+++ 2022/AdventOfCode2022/Day01/Program.cs
@@ -0,0 +1,5 @@
+﻿// https://adventofcode.com/2022/day/1
+
+var elves = File.ReadAllText("input.txt").Split("\r\n\r\n").Select(x => x.Split("\r\n").Select(int.Parse));
+Console.WriteLine(elves.Select(x => x.Sum()).Max());
+Console.WriteLine(elves.Select(x => x.Sum()).Order().Reverse().Take(3).Sum());
