81a3803c48abbfa273dbee53b0e3fad2b85c0bce
Jon Øystein <jon.softeland@gmail.com>
2022-12-04 06:55:30 +0100

    Day04

--- 2022/Day04/Program.cs
+++ 2022/Day04/Program.cs
@@ -3,11 +3,11 @@
 var input = File.ReadAllLines("input.txt")
     .Select(x => x.Split(',').Select(y => y.Split('-').Select(int.Parse).ToArray()).ToArray());
 
-var part1 = input.Select(x => 
+var part1 = input.Where(x => 
     (x[0][0] <= x[1][0] && x[0][1] >= x[1][1]) ||  
     (x[1][0] <= x[0][0] && x[1][1] >= x[0][1]) );
 
-var part2 = input.Select(x => (x[0][1] >= x[1][0]) && (x[1][1] >= x[0][0]));
+var part2 = input.Where(x => (x[0][1] >= x[1][0]) && (x[1][1] >= x[0][0]));
 
-Console.WriteLine(part1.Count(x => x == true));
-Console.WriteLine(part2.Count(x => x == true));
+Console.WriteLine(part1.Count());
+Console.WriteLine(part2.Count());
