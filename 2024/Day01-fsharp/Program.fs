60b3668debebc59669d20e667166c5658e70ea94
Jon Øystein <24587666+jonsofte@users.noreply.github.com>
2024-12-01 10:46:45 +0100

    2024 - Day01

---
+++ 2024/Day01-fsharp/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open System.IO
+
+let input = "input.txt" |> File.ReadAllLines |> Seq.map (fun t -> t.Split(' ',StringSplitOptions.RemoveEmptyEntries))
+
+let firstColumn = input |> Seq.map (fun x -> int x[0]) |> Seq.sort
+let secondColumn = input |> Seq.map (fun x -> int x[1]) |> Seq.sort
+
+let part1 = Seq.zip firstColumn secondColumn |> Seq.map (fun (f,s) -> abs (f-s)) |> Seq.sum
+let part2 = firstColumn |> Seq.map (fun f -> (secondColumn |> Seq.filter (fun s -> f=s) |> Seq.length) * f) |> Seq.sum 
+
+printfn "%d" part1
+printfn "%d" part2 
