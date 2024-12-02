c93dcaae52a08720c2644b26b032a0bb0f219817
Jon Øystein <24587666+jonsofte@users.noreply.github.com>
2024-12-02 22:31:32 +0100

    2024 - Day02

--- 2024/Day01-fsharp/Program.fs
+++ 2024/Day01-fsharp/Program.fs
@@ -6,7 +6,7 @@ let input = "input.txt" |> File.ReadAllLines |> Seq.map (fun t -> t.Split(' ',St
 let firstColumn = input |> Seq.map (fun x -> int x[0]) |> Seq.sort
 let secondColumn = input |> Seq.map (fun x -> int x[1]) |> Seq.sort
 
-let part1 = Seq.zip firstColumn secondColumn |> Seq.map (fun (f,s) -> abs (f-s)) |> Seq.sum
+let part1 = (firstColumn,secondColumn) ||> Seq.zip  |> Seq.map (fun (f,s) -> abs (f-s)) |> Seq.sum
 let part2 = firstColumn |> Seq.map (fun f -> (secondColumn |> Seq.filter (fun s -> f=s) |> Seq.length) * f) |> Seq.sum 
 
 printfn "%d" part1
---
+++ 2024/Day02-fsharp/Program.fs
@@ -0,0 +1,25 @@
+﻿// https://adventofcode.com/2024/day/2
+
+open System.IO
+
+let input = "input.txt" |> File.ReadAllLines |> Seq.map (fun t -> t.Split(' ') |> Seq.map int)
+
+let isSorted report = 
+    (report,Seq.sort report) ||> Seq.compareWith Operators.compare = 0 || 
+    (report,Seq.sortDescending report) ||> Seq.compareWith Operators.compare = 0
+
+let invalidLevel report = 
+    (report |> Seq.skip 1, report) ||> Seq.zip |> Seq.map (fun (f,s) -> abs (f-s)) 
+    |> Seq.exists (fun x -> x>3 || x=0)
+
+let isSafe report = isSorted report && not (invalidLevel report)
+
+let isSafeWithSingleBadLevel report =
+    [0..report |> Seq.length] 
+    |> Seq.map (fun postion -> report |> Seq.indexed |> Seq.filter (fun (i,_) -> i <> postion) |> Seq.map (fun (_,v) -> v))
+    |> Seq.exists (fun r -> isSafe r)
+
+let getNumberOfReports filter input = input |>  Seq.map filter |> Seq.filter (fun x -> x) |> Seq.length 
+
+printfn "%d" (input |> getNumberOfReports isSafe)
+printfn "%d" (input |> getNumberOfReports isSafeWithSingleBadLevel)
