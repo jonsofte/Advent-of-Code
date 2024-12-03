afe23f34bc5bcd0574fbdff285c71038db30a147
Jon Øystein <24587666+jonsofte@users.noreply.github.com>
2024-12-03 22:37:24 +0100

    2024 - Day03

---
+++ 2024/Day03-fsharp/Program.fs
@@ -0,0 +1,25 @@
+﻿// https://adventofcode.com/2024/day/3
+
+open System.IO
+open System.Text.RegularExpressions;
+
+let input = "input.txt" |> File.ReadAllText
+let mul (m: Match) = int m.Groups[1].Value * int m.Groups[2].Value
+
+let mulMatches = Regex.Matches(input,"mul\((\d+),(\d+)\)")
+let part1 = mulMatches |> Seq.map (fun m -> mul m) |> Seq.sum
+
+type result = {Enabled: bool; Sum: int}
+
+let calculate current (value: Match) = 
+    match (current.Enabled,value.Value) with
+    | _,"don't()" -> {Enabled = false; Sum = current.Sum}
+    | _,"do()" -> {Enabled = true; Sum = current.Sum}
+    | true,_ -> {Enabled = true; Sum = current.Sum + mul(value)}
+    | false,_ -> {Enabled = false; Sum = current.Sum}
+
+let allMatches = Regex.Matches(input,"mul\((\d+),(\d+)\)|do[(][)]|don't[(][)]")
+let part2 = (allMatches |> Seq.fold calculate {Enabled = true; Sum=0}).Sum
+
+printfn "%d" part1
+printfn "%d" part2
