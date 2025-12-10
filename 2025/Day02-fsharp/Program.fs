93989178128ef997c17d6fa206dc18cecbc21629
Jon Øystein <24587666+jonsofte@users.noreply.github.com>
2025-12-10 22:52:43 +0100

    2025 - Day02

--- 2025/Day02-fsharp/Program.fs
+++ 2025/Day02-fsharp/Program.fs
@@ -7,16 +7,17 @@ let getRanges file =
     |> Seq.map(fun s -> s.Split('-')) 
     |> Seq.map(fun p -> (int64 p[0], int64 p[1]))
 
-let splitString split (value: string) =
-    if (value.Length < split) || value.Length % split <> 0 then
+let splitString n (value: string) =
+    if (value.Length < n) || value.Length % n <> 0 then
         Seq.empty
     else 
         value
-        |> Seq.chunkBySize (value.Length / split)
+        |> Seq.chunkBySize (value.Length / n)
         |> Seq.map (fun s -> new string(s))
 
 let isInvalid split value =
-    splitString split (string value) 
+    string value
+    |> splitString split  
     |> Seq.distinct 
     |> Seq.length = 1
 
@@ -29,13 +30,13 @@ let part1 =
     |> Seq.collect (getInvalidValues (isInvalid 2))
     |> Seq.sum 
 
-let isInvalidAllSplits value = 
+let isValidAllLengths value = 
     [2..(string value).Length] 
     |> Seq.exists (fun i -> isInvalid i value)
 
 let part2 =
     getRanges "input.txt"
-    |> Seq.collect (getInvalidValues isInvalidAllSplits)
+    |> Seq.collect (getInvalidValues isValidAllLengths)
     |> Seq.sum 
 
 printfn "%d" part1
